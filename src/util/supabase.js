ac7c1d1b8485a408f639cc93496c3991ac0ab3c0
_kei_ <116942403+Gcoyy@users.noreply.github.com>
2025-09-27 17:50:12 +0800

    MINOR UPDATE: Fixed the db

--- src/util/supabase.js
+++ src/util/supabase.js
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
+
 const supabaseUrl = 'https://nvgopjdirsatrlxkinnn.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52Z29wamRpcnNhdHJseGtpbm5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg5NjQwNzMsImV4cCI6MjA3NDU0MDA3M30.VaoXiqfOfGAjFyWhZUPhfz6x_qBlbEgUo3TSGlqUYVw'
-const supabase = createClient(supabaseUrl, supabaseKey)
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
