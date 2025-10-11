d26936f7d367dcdf12076dc685fd47d073369a8b
Grimes. <glanrtomaroy@su.edu.ph>
2025-10-11 10:50:29 +0800

    Update supabase.js

--- src/util/supabase.js
+++ src/util/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js' // import for database integration
 
 const supabaseUrl = 'https://nvgopjdirsatrlxkinnn.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52Z29wamRpcnNhdHJseGtpbm5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg5NjQwNzMsImV4cCI6MjA3NDU0MDA3M30.VaoXiqfOfGAjFyWhZUPhfz6x_qBlbEgUo3TSGlqUYVw'
